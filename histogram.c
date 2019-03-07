c8350301ab86fab274d08b3484dd4875093f05f5
ASHOK-SINGH <37559719+XLAR-8@users.noreply.github.com>
2019-03-07 20:03:11 +0530

    Update histogram.c

--- histogram.c
+++ histogram.c
@@ -1,109 +1,126 @@
 /**********************************************************************
-**  histogram.c
-**	Histogramm from Memory Buffer
-**	Version 1.00
+**  cmv_reg.h
+**	Register Definitions
+**	Version 1.1
 **
-**  Copyright (C) 2019 Ashok Singh
+**  Copyright (C) 2013-2014 H.Poetzl
 **
 **	This program is free software: you can redistribute it and/or
 **	modify it under the terms of the GNU General Public License
 **	as published by the Free Software Foundation, either version
 **	2 of the License, or (at your option) any later version.
 **
-**  Refrence : https://github.com/apertus-open-source-cinema/axiom-beta-firmware/blob/master/software/sensor_tools/hist/hist.c 
 **********************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef	_CMV_REG_H_
+#define	_CMV_REG_H_
 
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/time.h>
+#ifdef	REG_DELAY
+#define	delay(n)	usleep(REG_DELAY * n)
+#else
+#define	delay(n)	do { } while (0)
+#endif
 
-static u_int16_t num_cols = 4096;
-static u_int16_t num_rows = 3072;
+uint32_t cmv_addr = 0x00000000;
 
-int main(int argc, char *argv[])
+uint16_t get_cmv_reg(unsigned reg)
 {
-    struct timeval  tv1, tv2;
-    gettimeofday(&tv1, NULL);
-    
-     
-    int hist[4][32] = {{ 0 }};
-    u_int8_t *buffer;
-
-    struct stat st;
-    size_t size;
-
-    int fd = open("sources/index.raw12", O_RDWR);
-    if (fd == -1) {
-	    printf("error opening FILE\n");
-	    exit(1);
-	}
-    
-    int rc = fstat(fd, &st);
-    size=st.st_size;
-
-    buffer = mmap(0, size, PROT_READ|PROT_EXEC,
-                  MAP_SHARED, fd, 0);
-    if (buffer == (u_int8_t *)-1) {
-	    printf("error mapping ");
-	    exit(2);
-	} 
- 
-    int length = num_rows * num_cols * (3)/2;       /* 2 - 12 bits equals to 3 - 8 bits */
-  
-    for(int i=0; i<length; i+=3){               
-       if (i%(3*(8092)/2) < 4096*(3)/2) {       /* ODD RAW */
-          hist[0][(buffer[i] >> 3)]++;          /* CH[0]  */
-          hist[1][((buffer[i+1] & 0x0F << 1) | 
-          (buffer[i+2] >> 7))]++;               /* CH[1]  */
-        } else {                                /* EVEN   */
-        hist[2][( buffer[i] >> 3 )]++ ;         /* CH[2]  */
-        hist[3][((buffer[i+1] & 0x0F << 1) |
-         (buffer[i+2] >> 7))]++ ;               /* CH[3]  */
-        }  
-    } 
-
-    printf("\n---------------------------------------------------");   
-    printf("\nBUCKET NO.    R1        G1       G2        B1");
-    
-    for(int i=0; i<32; i++) {
-       printf("\n%d        ",i);
-       printf("   =");
-
-       printf(" %d     %d     %d     %d ",
-              hist[0][i] ,hist[1][i] ,
-              hist[2][i] ,hist[3][i]);
-    } 
-  
-    printf("\n---------------------------------------------------");
-    printf("\n");
-
-    rc = munmap(buffer, size);
-    fprintf(stderr, "\n unmap() = %d\n", rc);
-    close(fd);
-
-    gettimeofday(&tv2, NULL);
-    printf ("\nTotal time = %f seconds\n",
-           (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
-           (double) (tv2.tv_sec - tv1.tv_sec));
-
-    return 0;
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00000000);
+	return ptr[reg] & 0xFFFF;
 }
-   //12582912
-    
-   
-   
-   
-   
 
-   
-   
-   
-   
-   
-   
-   
+void	set_cmv_reg(unsigned reg, uint16_t val)
+{
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00000000);
+	ptr[reg] = val;
+	delay(1);
+}
+
+
+
+uint32_t get_fil_reg(unsigned reg)
+{
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00100100);
+	return ptr[reg];
+}
+
+void	set_fil_reg(unsigned reg, uint32_t val)
+{
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00100100);
+	ptr[reg] = val;
+	delay(1);
+}
+
+enum _fil_reg {
+	FIL_REG_BUF0 = 0,
+	FIL_REG_PAT0 = 1,
+	FIL_REG_BUF1 = 2,
+	FIL_REG_PAT1 = 3,
+	FIL_REG_BUF2 = 4,
+	FIL_REG_PAT2 = 5,
+	FIL_REG_BUF3 = 6,
+	FIL_REG_PAT3 = 7,
+
+	FIL_REG_CINC = 8,
+	FIL_REG_RINC = 9,
+	FIL_REG_CCNT = 10,
+
+	FIL_REG_CONTROL = 11,
+
+	FIL_REG_PATTERN = 12,
+	FIL_REG_MASKVAL = 13,
+
+	FIL_REG_LEDOVR = 14,
+	FIL_REG_OVERRIDE = 15,
+
+	FIL_REG_USR_ACCESS = 65,
+	FIL_REG_MATCH = 66,
+	FIL_REG_MISMATCH = 67,
+	FIL_REG_ADDR = 68,
+	FIL_REG_STATUS = 69,
+	FIL_REG_CSEQ = 70,
+};
+
+
+
+
+uint32_t get_del_reg(unsigned reg)
+{
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00200000);
+	return ptr[reg];
+}
+
+void	set_del_reg(unsigned reg, uint32_t val)
+{
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00200000);
+	ptr[reg] = val;
+	delay(5);
+}
+
+
+uint32_t get_rcn_col(unsigned col)
+{
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00302000);
+	return ptr[(col/2) - (col&1)*0x800];
+}
+
+void	set_rcn_col(unsigned col, uint32_t val)
+{
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00302000);
+	ptr[(col/2) - (col&1)*0x800] = val;
+}
+
+uint32_t get_rcn_row(unsigned row)
+{
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00304000);
+	return ptr[(row/2) + (row&1)*0x800];
+}
+
+void	set_rcn_row(unsigned row, uint32_t val)
+{
+	volatile uint32_t *ptr = (uint32_t *)(cmv_addr + 0x00304000);
+	ptr[(row/2) + (row&1)*0x800] = val;
+}
+
+
+#endif	/* _CMV_REG_H_ */
