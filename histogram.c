18d602db554c73ee50d3fcd14247421cbc7bfd2e
ASHOK-SINGH <37559719+XLAR-8@users.noreply.github.com>
2019-03-07 20:16:01 +0530

    Update histogram.c

--- histogram.c
+++ histogram.c
@@ -5,10 +5,10 @@
 **
 **  Copyright (C) 2019 Ashok Singh
 **
-**   This program is free software: you can redistribute it and/or
-**   modify it under the terms of the GNU General Public License
-**   as published by the Free Software Foundation, either version
-**   2 of the License, or (at your option) any later version.
+**  This program is free software: you can redistribute it and/or
+**  modify it under the terms of the GNU General Public License
+**  as published by the Free Software Foundation, either version
+**  2 of the License, or (at your option) any later version.
 **
 **  Refrence : https://github.com/apertus-open-source-cinema/axiom-beta-firmware/blob/master/software/sensor_tools/hist/hist.c 
 **********************************************************************/
