95937a58b5f72abcb5b9cf9c8390a25235534264
ASHOK-SINGH <37559719+XLAR-8@users.noreply.github.com>
2019-03-17 05:17:14 +0530

    update histogram.c

--- histogram.c
+++ histogram.c
@@ -25,17 +25,52 @@
 static u_int16_t num_cols = 4096;
 static u_int16_t num_rows = 3072;
 
+
+void histogram(u_int8_t *buffer)
+{   
+    int hist[4][32] = {{ 0 }};
+  
+    /* 2 - 12 bits equals to 3 - 8 bits */
+    int length = num_rows * num_cols * (3)/2;        
+  
+    /*converting 12 bit to 5 bit and hashing */ 
+    for(int i = 0; i < length; i += 3){               
+       if (i % (3 * 2 * num_cols / 2) < num_cols * (3) / 2) {       
+          hist[0][buffer[i] >> 3]++;                                /* CH[0] */
+          hist[1][buffer[i+1] & 0x0F << 1 |                         /* CH[1] */
+                   buffer[i+2] >> 7]++;          
+        } else {                                                    
+          hist[2][buffer[i] >> 3]++;                                /* CH[2] */
+          hist[3][buffer[i+1] & 0x0F << 1 |                         /* CH[3] */
+                buffer[i+2] >> 7]++ ;               
+        }  
+    } 
+
+    printf("\n-----------------------------------------------");   
+    printf("\nBUCKET NO.    R1        G1       G2        B1");
+    
+    for(int i = 0; i < 32; ++i) {
+       printf("\n%2d   ",i);
+       printf(" =  ");
+       printf(" %5d     %5d     %5d     %5d ",
+              hist[0][i], hist[1][i],
+              hist[2][i], hist[3][i]);
+    } 
+  
+    printf("\n------------------------------------------------");
+}
+
+
 int main(int argc, char *argv[])
 {
     struct timeval  tv1, tv2;
-    gettimeofday(&tv1, NULL);
-    
-    int hist[4][32] = {{ 0 }};
-    u_int8_t *buffer;
 
     struct stat st;
     size_t size;
 
+    u_int8_t *buffer;
+
+    /* Opening raw12 file with read/write permission */
     int fd = open("sources/index.raw12", O_RDWR);
     if (fd == -1) {
 	    printf("error opening FILE\n");
@@ -45,6 +80,7 @@ int main(int argc, char *argv[])
     int rc = fstat(fd, &st);
     size=st.st_size;
 
+    /* Mapping buffer for memory allocation */
     buffer = mmap(0, size, PROT_READ|PROT_EXEC,
                   MAP_SHARED, fd, 0);
     if (buffer == (u_int8_t *)-1) {
@@ -52,43 +88,19 @@ int main(int argc, char *argv[])
 	    exit(2);
 	} 
  
-    int length = num_rows * num_cols * (3)/2;                           /* 2 - 12 bits equals to 3 - 8 bits */
+    gettimeofday(&tv1, NULL);
   
-    for(int i = 0; i < length; i += 3) {               
-       if (i % (3 * 8092 / 2) < 4096 * (3) / 2) {                      /* ODD ROW */
-          hist[0][(buffer[i] >> 3)]++;                            /* CH[0] */
-          hist[1][(buffer[i + 1] & 0x0F << 1 |            /* CH[1] */
-                   buffer[i + 2] >> 7)]++;          
-        } else {                                        /* EVEN ROW*/
-          hist[2][(buffer[i] >> 3)]++;                  /* CH[2] */
-          hist[3][(buffer[i + 1] & 0x0F << 1 |            /* CH[3] */
-                (buffer[i + 2] >> 7))]++ ;               
-        }  
-    } 
-
-    printf("\n-----------------------------------------------");   
-    printf("\nBUCKET NO.    R1        G1       G2        B1");
-    
-    for(int i = 0; i < 32; ++i) {
-       printf("\n%d        ",i);
-       printf("   =");
-       printf(" %d     %d     %d     %d ",
-              hist[0][i], hist[1][i],
-              hist[2][i], hist[3][i]);
-    } 
+    histogram(buffer);
   
-    printf("\n------------------------------------------------");
-    printf("\n");
+    gettimeofday(&tv2, NULL);
 
     rc = munmap(buffer, size);
     printf("\n unmap() = %d\n", rc);
     close(fd);
 
-    gettimeofday(&tv2, NULL);
     printf("\nTotal time = %f seconds\n",
           (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
           (double) (tv2.tv_sec - tv1.tv_sec));
 
     return 0;
 }
-   //12582912 
