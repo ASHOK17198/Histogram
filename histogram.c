29036a50d93cd0056f8aeec3e64703b2634d0a89
ASHOK-SINGH <37559719+XLAR-8@users.noreply.github.com>
2019-03-28 12:31:30 +0530

    Update histogram.c

--- histogram.c
+++ histogram.c
@@ -1,16 +1,17 @@
 /**********************************************************************
 **  histogram.c
-**  Histogramm from Memory Buffer
-**  Version 1.00
+**  	Histogramm from Memory Buffer
+**  	
+**      Version 1.00
 **
 **  Copyright (C) 2019 Ashok Singh
 **
-**  This program is free software: you can redistribute it and/or
-**  modify it under the terms of the GNU General Public License
-**  as published by the Free Software Foundation, either version
-**  2 of the License, or (at your option) any later version.
+**      This program is free software: you can redistribute it and/or
+**   	modify it under the terms of the GNU General Public License
+**  	as published by the Free Software Foundation, either version
+**  	2 of the License, or (at your option) any later version.
 **
-**  Refrence : https://github.com/apertus-open-source-cinema/axiom-beta-firmware/blob/master/software/sensor_tools/hist/hist.c 
+**  Refrence : https://github.com/apertus-open-source-cinema/axiom-beta-firmware/blob/master/software/sensor_tools 
 **********************************************************************/
 
 #include <stdio.h>
@@ -20,55 +21,81 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/time.h>
+#include <time.h>
+#define	CLOCK_ID    CLOCK_REALTIME
 
 static u_int16_t num_cols = 4096;
 static u_int16_t num_rows = 3072;
 
+static inline
+double	delta_ns(struct timespec *a, struct timespec *b)
+{
+	double delta = b->tv_nsec - a->tv_nsec;
 
-void histogram(u_int8_t *buffer)
-{   
-    int hist[4][32] = {{ 0 }};
-  
-    /* 2 - 12 bits equals to 3 - 8 bits */
-    int length = num_rows * num_cols * (3)/2;        
-  
-    /*converting 12 bit to 5 bit and hashing */ 
-    for(int i = 0; i < length; i += 3){               
-       if (i % (3 * 2 * num_cols / 2) < num_cols * (3) / 2) {       
-          hist[0][buffer[i] >> 3]++;                                /* CH[0] */
-          hist[1][buffer[i+1] & 0x0F << 1 |                         /* CH[1] */
-                   buffer[i+2] >> 7]++;          
-        } else {                                                    
-          hist[2][buffer[i] >> 3]++;                                /* CH[2] */
-          hist[3][buffer[i+1] & 0x0F << 1 |                         /* CH[3] */
-                buffer[i+2] >> 7]++ ;               
-        }  
-    } 
-
-    printf("\n-----------------------------------------------");   
-    printf("\nBUCKET NO.    R1        G1       G2        B1");
-    
-    for(int i = 0; i < 32; ++i) {
-       printf("\n%2d   ",i);
-       printf(" =  ");
-       printf(" %5d     %5d     %5d     %5d ",
-              hist[0][i], hist[1][i],
-              hist[2][i], hist[3][i]);
-    } 
-  
-    printf("\n------------------------------------------------");
+	return delta + (b->tv_sec - a->tv_sec) * 1e9;
 }
 
+static inline
+double	ns_to_s(double val)
+{
+	return val * 1e-9;
+}
+
+static inline
+double	delta_s(struct timespec *a, struct timespec *b)
+{
+	return ns_to_s(delta_ns(a, b));
+}
+
+void histogram(u_int64_t *buffer)
+{   
+    int hist[4][32] = {{ 0 }};
+    u_int64_t *ptr = (u_int64_t*)buffer; 
+    __int128_t reservoir = 0;
+    
+    // bits in reservoir
+    int bits = 0; 
+
+    //taking 64-bits at a time to reduce cache expense
+    for (int j = 0; j < num_rows; ++j) {
+        for (int i = 0; i < num_cols / 2; ++i) {
+            if (bits < 24) {
+                reservoir = (reservoir << 64) | htobe64(*ptr++);
+                bits += 64;
+            }
+
+            u_int8_t x = (reservoir >> (bits - 5)) & 31;
+            bits -= 12;
+            u_int8_t y = (reservoir >> (bits - 5)) & 31;
+            bits -= 12;
+            
+            if (j % 2 != 0) {
+                hist[0][x]++;       /* CH[0] */
+                hist[1][y]++;       /* CH[1] */
+            }
+            else {
+                hist[2][x]++;       /* CH[2] */
+                hist[3][y]++;       /* CH[3] */
+            } 
+        }
+    }
+    printf("\n--------------------------------------------");   
+    printf("\nBUCKET NO.    R1         G1         G2         B1");
+    for(int i = 0; i < 32; ++i) {
+        printf("\n%2d   ",i);
+        printf(" =  ");
+        printf(" %7d     %7d     %7d     %7d ",
+               hist[0][i], hist[1][i],
+               hist[2][i], hist[3][i]);
+    } 
+    printf("\n---------------------------------------------");
+}
 
 int main(int argc, char *argv[])
 {
-    struct timeval  tv1, tv2;
-
+    struct timespec time_base , now;
     struct stat st;
     size_t size;
-
-    u_int8_t *buffer;
 
     /* Opening raw12 file with read/write permission */
     int fd = open("sources/index.raw12", O_RDWR);
@@ -81,26 +108,20 @@ int main(int argc, char *argv[])
     size=st.st_size;
 
     /* Mapping buffer for memory allocation */
-    buffer = mmap(0, size, PROT_READ|PROT_EXEC,
-                  MAP_SHARED, fd, 0);
-    if (buffer == (u_int8_t *)-1) {
-	    printf("error mapping ");
-	    exit(2);
-	} 
- 
-    gettimeofday(&tv1, NULL);
-  
+    void *buffer = mmap(0, size, PROT_READ|PROT_EXEC,
+                        MAP_SHARED, fd, 0);
+   
+    /*get time for running histogram function */
+    clock_gettime(CLOCK_ID, &time_base);
     histogram(buffer);
-  
-    gettimeofday(&tv2, NULL);
-
+    clock_gettime(CLOCK_ID, &now);
+    
+    double delta = delta_s(&time_base, &now);
+    printf("\n time = %f",delta);
+    
     rc = munmap(buffer, size);
     printf("\n unmap() = %d\n", rc);
     close(fd);
-
-    printf("\nTotal time = %f seconds\n",
-          (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
-          (double) (tv2.tv_sec - tv1.tv_sec));
 
     return 0;
 }
